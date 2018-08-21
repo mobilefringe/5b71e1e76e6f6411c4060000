f0401b03a18d6dd5a03d427df1c2758eea5d916d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-08-21 11:32:09 -0400

    Long Beach Towne Center 2.0 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -49,7 +49,6 @@
                             </div>
                         </div>
                         <div class="col-md-9">
-                            <!--<mapplic-png-map ref="pngmap_ref" :height="664" :hovertip="true" :storelist="allStores" :floorlist="floorList" :bindLocationOpened="true" :svgWidth="property.map_image_width" :svgHeight="property.map_image_height" :showPin="true" tooltiplabel="View Store Details"></mapplic-png-map>-->
                             <mapplic-map ref="mapplic_ref" :height="664" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="allStores" :floorlist="floorList" tooltiplabel="View Store Details"></mapplic-map>
                         </div>
                     </div>

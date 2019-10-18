182c746502a06dae0a251f92c698cb34bac632c9
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-18 14:06:22 -0400

    Long Beach Towne Center 2.0 1.1  /center_information.vue

--- center_information.vue
+++ center_information.vue
@@ -42,13 +42,19 @@
                     <!--    </div>-->
                     <!--</div>-->
                 </div>
-                <div class="location_image_container margin_60">
-                    <div class="location_image" v-if="pageImages" v-for="item in pageImages">
-                        <img :src="item.image_url" alt="" />   
-                    </div>
-                </div>
+                <!--<div class="location_image_container">-->
+                <!--    <div v-if="lowerBanner">-->
+                <!--        <div class="location_banner" v-bind:style="{ background: 'linear-gradient(0deg, rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(' + lowerBanner.image_url + ') center center fixed' }"></div>-->
+                <!--        <h1 v-if="lowerBanner.name">{{ lowerBanner.name }}</h1>-->
+                <!--    </div>-->
+                <!--</div>-->
             </div>
         </transition>
+        <div class="location_image_container margin_60">
+            <div class="location_image" v-if="pageImages" v-for="item in pageImages">
+                <img :src="item.image_url" alt="" />   
+            </div>
+        </div>
     </div>
 </template>
 

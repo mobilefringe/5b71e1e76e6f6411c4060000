d7f75ab9b7a2ce9900e9908c6c4831b8c997caed
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-04 15:14:30 -0500

    Long Beach Towne Center 2.0 1.2  /home.vue

--- home.vue
+++ home.vue
@@ -45,7 +45,8 @@
             					            <h3>{{ item.name }}</h3>
         					            </span>
                 					    <span class="info">
-            					            <p><span v-if="isMultiDay(item)">{{ item.start_date | moment("MMMM D", timezone)}} to {{ item.end_date | moment("MMMM D", timezone)}}</span><span v-else>{{ item.start_date | moment("MMMM D", timezone)}}</span></p>
+            					            <p v-if="isMultiDay(item)">{{ item.start_date | moment("MMMM D", timezone)}} to {{ item.end_date | moment("MMMM D", timezone)}}</p>
+            					            <p v-else>{{ item.start_date | moment("MMMM D", timezone)}}</p>
             					            <p>View Event Details <i class="fa fa-angle-double-right" aria-hidden="true"></i></p>
         					            </span>
                     				</div>

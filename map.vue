e146d3f9e1fc0908beae0472ffbfd6326c8a793c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-04 15:59:54 -0500

    Long Beach Towne Center 2.0 1.2  /map.vue

--- map.vue
+++ map.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="inside_page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
                     <div class="main_container position_relative">
-                        <h2>Center Map</h2>
+                        <h1>Center Map</h1>
                     </div>
                 </div>
                 <div class="main_container">

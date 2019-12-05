3c16c08b500f3f4f7ff79788997628b961374083
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-05 14:08:23 -0500

    Long Beach Towne Center 2.0 1.2  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="inside_page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
                     <div class="main_container position_relative">
-                        <h2>Pop-up Specialty Leasing</h2>
+                        <h1>Pop-up Specialty Leasing</h1>
                     </div>
                 </div>
                 <div class="main_container">

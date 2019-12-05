7f2b9b53efbb17bb404e9c823884f7e6911462a4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-05 14:16:09 -0500

    Long Beach Towne Center 2.0 1.2  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -45,7 +45,7 @@
 </template>
 
 <script>
-	define(["Vue", "vuex"], function(Vue, Vuex) {
+	define(["Vue", "vuex", "json!site.json"], function(Vue, Vuex, Site) {
 		return Vue.component("location-component", {
             template: template, // the variable template will be injected
             data: function () {
@@ -95,7 +95,10 @@
                 loadData: async function () {
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/cerritos-leasing.json"})]);
+                        let results = await Promise.all([
+                            this.$store.dispatch("getData", "repos"), 
+                            this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + Site.subdomain + "-pop-ups-specialty-leasing.json" });
+                        ]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);

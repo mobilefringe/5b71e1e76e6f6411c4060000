5658119f4b59202be9929556c4ae72d798f7e19b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-05 14:22:20 -0500

    Long Beach Towne Center 2.0 1.2  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -16,17 +16,17 @@
                             <form class="newsletter_form form-horizontal" action="//mobilefringe.createsend.com/t/d/s/kuuudh/" method="post">
                                 <div class="row">
                                     <div class="col-sm-6" >
-                                        <label for="fieldzkydut" class="accessibility">First Name</label>
+                                        <label for="fieldzkydut" class="visuallyhidden">First Name</label>
                                         <input v-model="form_data.first_name" required class="margin_20 form-control" id="fieldzkydut" name="cm-f-zkydut" type="text" placeholder="First Name">
                                     </div>
                                     <div class="col-sm-6" >
-                                        <label for="fieldzkydui" class="accessibility">Last Name</label>
+                                        <label for="fieldzkydui" class="visuallyhidden">Last Name</label>
                                         <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldzkydui" name="cm-f-zkydui" type="text" placeholder="Last Name">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-12">
-                                        <label for="newsletter_email" class="accessibility">Email</label>
+                                        <label for="newsletter_email" class="visuallyhidden">Email</label>
                                         <input v-model="form_data.email" required class="margin_20 form-control" name="cm-kuuudh-kuuudh" type="email" placeholder="Email" id="newsletter_email">
                                     </div>
                                 </div>

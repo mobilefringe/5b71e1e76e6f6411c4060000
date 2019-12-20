116841e49fe71751c353a25402daf278b7db7123
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-20 13:23:30 -0500

    Long Beach Towne Center 2.0 1.2  /contest.vue

--- contest.vue
+++ contest.vue
@@ -70,6 +70,12 @@
                                                 I agree to receive newsletters from {{ property.name }}. (You can unsubscribe at anytime)
                                         </label>
         						    </div>
+        						     <div class="col-xs-12" :class="{'has-error': errors.has('agree_terms')}">
+        						        <label class="checkbox">
+                                            <input name="agree_terms" required type="checkbox" v-model="form_data.terms">
+                                                I have read and agree to the <a href="/pages/longbeach-rules-and-regulations" target="_blank">Contest Rules & Regulations</a>.
+                                        </label>
+        						    </div>
         						    <div class="col-xs-12">
         						        <p>For more details about personal privacy, please read our <a href="/pages/longbeach-privacy-policy" target="_blank">Privacy Policy</a>.</p>
                                     </div>

93f7a76274514d28dc126dd013a7da10b3b57512
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-20 13:30:05 -0500

    Long Beach Towne Center 2.0 1.2  /contest.vue

--- contest.vue
+++ contest.vue
@@ -73,13 +73,13 @@
         						     <div class="col-xs-12" :class="{'has-error': errors.has('agree_terms')}">
         						        <label class="checkbox">
                                             <input name="agree_terms" type="checkbox" v-model="form_data.terms" required>
-                                                I have read and agree to the <a href="/pages/longbeach-rules-and-regulations" target="_blank">Contest Rules & Regulations</a>.
+                                                I have read and agree to the <a class="primary" href="/pages/longbeach-rules-and-regulations" target="_blank">Contest Rules & Regulations</a>.
                                         </label>
         						    </div>
         					    </div>
         					    <div class="form-inline row margin_30">
         						    <div class="col-xs-12">
-        						        <p>For more details about personal privacy, please read our <a href="/pages/longbeach-privacy-policy" target="_blank">Privacy Policy</a>.</p>
+        						        <p>For more details about personal privacy, please read our <a class="primary" href="/pages/longbeach-privacy-policy" target="_blank">Privacy Policy</a>.</p>
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -96,8 +96,8 @@
     </div>
 </template>
 
-<style scoped>
-    a {
+<style>
+    a.primary {
         color: #5d7e67;
     }
 </style>
